52dcb5a0ed9a36d619489902ece880465c3a3f6d
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-10-10 01:59:55 -0300

    atualizaçao e estruturaçao do app

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, SafeAreaView } from 'react-native';
+
+import Filmes from './src/Filmes';
+
+import api from './src/services/api';
 
 export default function App() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes');
+      // console.log(response.data);
+
+      setFilmes(response.data)
+    }
+
+    loadFilmes();
+
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <Filmes data={item} />}
+      />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30
   },
 });
